42db87b1ea453688be103721901a3cae0f5f55df
Lisa Gunn <lisagunn@Lisas-MacBook-Pro.local>
2022-02-03 21:04:24 +0000

    added more js

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,63 @@
 var AP_KEY = "fa9ea468bf86f98d5f74d1ee663cf459";
+var displayContainer = document.querySelector('#display_container');
+var userForm = document.querySelector('#user-form')
+var input = document.querySelector('#input')
 
 
-var getCoordiantes = function (placeName) {
+var getCoordinates = function (placeName) {
     var apiUrl = 'https://api.openweathermap.org/geo/1.0/direct?q='+placeName+',*&limit=3&appid='+AP_KEY;
     fetch(apiUrl)
     .then(function (response) {
+      console.log('its ok')
       if (response.ok) {
         response.json().then(function (data) {
           displayPlaces(data);
-        });
-      } else {
-        alert('Error: ' + response.statusText);
-      }
-    })
+      });
+    }
+  })
 };
+  
+    var displayPlaces = function (possibleOptions){
+      if (possibleOptions.length===0){
+        displayContainer.textContent = 'No place matches your search';
+        return;
+      }
+    }
+var formSubmitHandler = function (event) {
+  event.preventDefault ();
+  var getCoordinates = input.value.trim();
+  if (getCoordinates) {
+    placeName(getCoordinates)
+    displayContainer.textContent='';}
+  else {
+    displayContainer.textContent='Please enter a place'
+  }
+  userForm.addEventListener('submit', formSubmitHandler);
+  }
+
+  for (var i = 0; i < possPlaces.length; i++) {
+    var placeName = possPlaces[i].name + '  ' + possPlaces[i].country;
+    var placeEl = document.createElement('li');
+    placeEl.classList = 'list-item flex-row justify-space-between align-center';
+    // Computer creates the choices as buttons so that the user can select one.
+    var titleEl = document.createElement('button');
+    titleEl.textContent = placeName;
+    titleEl.name='choice';
+    titleEl.id='choice'+i;
+    titleEl.value=i;
+    placeEl.appendChild(titleEl);
+    specifyContainerEl.appendChild(placeEl);
+  }
+
+ 
+
+
+
+
+
+
+
+
+
+
 
