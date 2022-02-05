1528e0bbf587f28a0b1ac46d0572ad95a4a4cbdb
LisaCR01 <lisagunn01@gmail.com>
2022-02-05 13:13:25 +0000

    made options work

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,16 @@ var displayContainer = document.querySelector('#display_container');
 var userForm = document.querySelector('#user-form')
 var input = document.querySelector('#input')
 
+var formSubmitHandler = function (event) {
+  event.preventDefault ();
+  var placeName = input.value.trim();
+  if (placeName) {
+    getCoordinates(placeName)
+    displayContainer.textContent='';}
+  else {
+    displayContainer.textContent='Please enter a place'
+  }
+  };
 
 var getCoordinates = function (placeName) {
     var apiUrl = 'https://api.openweathermap.org/geo/1.0/direct?q='+placeName+',*&limit=3&appid='+OP_KEY;
@@ -15,34 +25,30 @@ var getCoordinates = function (placeName) {
     }
   })
 };
-  
-    var displayPlaces = function (possibleOptions){
-      if (possibleOptions.length===0){
-        displayContainer.textContent = 'No place matches your search';
+
+var displayPlaces = function (possibleOptions){
+    if (possibleOptions.length===0){
+      displayContainer.textContent = 'No place matches your search';
         return;
       }
+    
+     for (var i = 0; i < possibleOptions.length; i++) {
+        var placeName = possibleOptions[i].name + '  ' + possibleOptions[i].country;
+        var place = document.createElement('li');
+        // Computer creates the choices as buttons so that the user can select one.
+        var title = document.createElement('button');
+        title.textContent = placeName;
+        title.name='choice';
+        title.id='choice'+i;
+        title.value=i;
+        place.appendChild(title);
+        displayContainer.appendChild(place);
+      }
     }
-var formSubmitHandler = function (event) {
-  event.preventDefault ();
-  var getCoordinates = input.value.trim();
-  if (getCoordinates) {
-    placeName(getCoordinates)
-    displayContainer.textContent='';}
-  else {
-    displayContainer.textContent='Please enter a place'
-  }
-  };
 
-  for (var i = 0; i < possibleOptions.length; i++) {
-    var placeName = possibleOptions[i].name + '  ' + possibleOptions[i].country;
-    var place = document.createElement('li');
-    // Computer creates the choices as buttons so that the user can select one.
-    var title = document.createElement('button');
-    title.textContent = placeName;
-    title.name='choice';
-    title.id='choice'+i;
-    title.value=i;
-    place.appendChild(title);
-    displayContainer.appendChild(place);
-  }
+
+  userForm.addEventListener('submit', formSubmitHandler);
+
+
+
   
