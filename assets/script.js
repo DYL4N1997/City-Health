a0b70792d01f7abf6a1b01705ca267e2471f1325
LisaCR01 <lisagunn01@gmail.com>
2022-02-09 15:41:52 +0000

    added relevent js comments

--- assets/script.js
+++ assets/script.js
@@ -6,28 +6,40 @@ var airQualityEl = document.querySelector('#air-pollution');
 // locBtn is an object that will hold my selected place and its latitude and longitude.
 // an is a variable that contains many values
 var locBtn = new Object()
+// PcApi will be the url for finding region data for the COVID-19 app
 var pcApi="";
-var dataReg="";
+// covidApi will be the url for COVID-19 statistics
 var covidApi= "";
+// dataReg is the regional data fetched from PcApi
+var dataReg="";
+// dataCovR is the COVID-19 data  that contains the transmission rates in the searched area
 var dataCovR="";
+// dataCov is the number of COVID-19 cases in the searched area.
 var dataCov="";
+// dataCovHosp is the number of people in the searched area's hospitals at the time.
+var dataCovHosp="";
 
-//this moment is needed for covid API in this layout
-var now = moment().format('YYYY-MM-DD');
-now=moment().subtract(1, 'day').format('YYYY-MM-DD');
+//this moment is needed for covidAPI in this layout format for: dataCovHosp and dataCov
+var now=moment().subtract(1, 'day').format('YYYY-MM-DD');
+//this moment is needed for covidAPI in this layout format for: dataCovR as to access the information the date has to be the previous Friday.
 var prevFri=moment().day(-2).format('YYYY-MM-DD');
 
+// Form for user to submit of which place name they would like to see the COVID-19 and air pollution statistics.
 var formSubmitHandler = function (event) {
   event.preventDefault ();
   var placeName = input.value.trim();
   if (placeName) {
     getCoordinates(placeName)
     displayContainer.textContent='';}
+    // If the user does submits a blank location the computer prompts them to submit a name of a location.
   else {
     displayContainer.textContent='Please enter a place'
   }
   };
 
+// Computer selects upto three possible places that correspond to the user's input.
+// Computer uses the GEO API from openweathermap
+// Computer restricts places to Great Britain
 var getCoordinates = function (placeName) {
     var apiUrl = 'https://api.openweathermap.org/geo/1.0/direct?q='+placeName+',*,GB&limit=3&appid='+OP_KEY;
     fetch(apiUrl)
@@ -40,14 +52,17 @@ var getCoordinates = function (placeName) {
   })
 };
 
+// Computer creates a list of upto three possible places for the user to choose from.
 var displayPlaces = function (possibleOptions){
     if (possibleOptions.length===0){
+      // If computer can not find any places to match the user's input within Great Britain then the message below is displayed on the webpage.
       displayContainer.textContent = 'Please Choose a Place in Great Britain';
         return;
       }
       var placeGroup=document.querySelector("#place_group")
       placeGroup.textContent="Please select the desired place:";
       placeGroup.classList = 'font-semibold p-3 text-xl text-center text-green-900 '
+      // Computer generates the list to be visible to the user on the webpage.
      for (var i = 0; i < possibleOptions.length; i++) {
         var placeName = possibleOptions[i].name + '  ' + possibleOptions[i].country;
         var place = document.createElement('ul');
@@ -61,13 +76,9 @@ var displayPlaces = function (possibleOptions){
         place.appendChild(title);
         displayContainer.appendChild(place);
       }
-    
 
-    // when the user clicks on the place they want to select it extracts the related id and puts the place plus its lat and long in locBtn
+    // Computer finds all of the buttons when clicked on extracts the related id and puts the place and its latitude and longitude in locBtn.
     $("button[name='choice']").on('click',function() {
-      // parse int changes string to an integer
-      // in this case the integer (in this case it can be 0,1,2 depending on which button the user clicks)
-      // we change it to an integer so that the computer knows which place you chose and thus which information to come up with.
       var j=parseInt($(this).val());
       console.log(j+ "j")
       //storing properties of selected location so that you can now access name, lat and long 
@@ -75,7 +86,7 @@ var displayPlaces = function (possibleOptions){
       locBtn.lat=possibleOptions[j].lat; 
       locBtn.lon=possibleOptions[j].lon;
       document.querySelector("#Area").innerHTML = locBtn.place
-      console.log('hello')
+       // Computer calls the open weather air quality API
       getCityAirQuality(locBtn);
       
       // this API find the local authority (it associates the lat and lon with local gov region needed for covid nhs api)
@@ -84,9 +95,12 @@ var displayPlaces = function (possibleOptions){
       
   getRegion(pcApi)}
       );
-      //make sure inside the bracket that opens at the possible options function
   };
   
+// pcApi is a necessary to be able to use the gov COVID-19 statistics because of how the statistics are grouped
+// It also means you can search areas that are under local authority to the larger NHS regions 
+// they are all classified by the office for National statistics geographical codes
+// National statistics geographical codes begin with an e and followed by a series of digits
   var getRegion = function (pcApi) {
       fetch(pcApi)
         .then(function (response) {
@@ -100,12 +114,14 @@ var displayPlaces = function (possibleOptions){
                 if(dataReg.included[3].relationships.areatype.data.id=="laua"){
                   placeCode=dataReg.included[3].id;regionCode=dataReg.included[6].id}
                   else{placeCode=dataReg.included[2].id;regionCode=dataReg.included[5].id};
-                //
                   covidApi='https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=utla;areaCode='+placeCode+';date='+now+'&structure={"name":"areaName","areaCode":"areaCode","date":"date","dailyCases": "newCasesByPublishDate"}&latestBy:"newCasesByPublishDate"';
                   covidApiHosp='https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nhsRegion;areaCode='+regionCode+';date='+now+'&structure={"name":"areaName","areaCode":"areaCode","date":"date","hospitalCases":"hospitalCases","transmissionRateMax":"transmissionRateMax"}&latestBy:"newCasesByPublishDate"';
                   covidApiR='https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nhsRegion;areaCode='+regionCode+';date='+prevFri+'&structure={"name":"areaName","areaCode":"areaCode","date":"date","hospitalCases":"hospitalCases","transmissionRateMax":"transmissionRateMax"}&latestBy:"newCasesByPublishDate"';
+                  // getCovidCases pulls the data from covidApi
                  getCovidCases(covidApi);
+                 // getCovidHospital  pulls the data from covidApiHosp
                  getCovidHospital(covidApiHosp);
+                 // getCovidR pulls the data from covidApiR
                  getCovidR(covidApiR);
             });
           } 
@@ -120,6 +136,7 @@ var getCovidCases = function (covidApi) {
             response.json().then(function (data) {
               // today's data from the Gov about COVID stats for the selected place
               dataCov=data
+              // COVID-19 cases are shown inside the id of: covid-cases in the HTML
               document.querySelector("#covid-cases").innerHTML= " Daily Cases: "+ dataCov.data[0].dailyCases;
             });
         } 
@@ -133,8 +150,7 @@ var getCovidHospital = function (covidApiHosp) {
             response.json().then(function (data) {
               // today's data from the Gov about COVID stats for the selected place
               dataCovHosp=data;
-              console.log(dataCovHosp);
-              console.log("area "+ dataCov.data[0].name + " daily cases "+ dataCovHosp.data[0].dailyCases)
+              // COVID-19 hospital cases are shown inside the id of: hospital-occupancy in the HTML
               document.querySelector("#hospital-occupancy").innerHTML= " Hospital Occupancies: "+ dataCovHosp.data[0].hospitalCases;
             });
         } 
@@ -148,33 +164,26 @@ var getCovidR = function (covidApiR) {
             response.json().then(function (data) {
               // today's data from the Gov about COVID stats for the selected place
               dataCovR=data;
-              console.log(dataCovR);
-              console.log("area "+ dataCov.data[0].name + " daily cases "+ dataCovHosp.data[0].dailyCases)
+              // COVID-19 transmission rate is shown inside the id of: r-rate in the HTML
               document.querySelector("#r-rate").innerHTML="R rate: "+ dataCovR.data[0].transmissionRateMax;
             });
         } 
       });
     };
 
-// locBtn holds the lat and lon so when you put it as the argument then when you call for the lat and lon it spits it back
+// Computer gets the air quality data for the selected place by using locBtn
 function getCityAirQuality (locBtn) {
-  //lon and lat the other way round (look at the = sign infront to know which one is which)
-  //put locBtn.lat/locBtn.lon in the correct spaces
-  //changed apiUrl to be more specific
+  // /apiAQUrl will be the url for finding the air quality 
   var apiAQUrl = 'https://api.openweathermap.org/data/2.5/air_pollution?lat='+locBtn.lat+'&lon='+locBtn.lon+'&appid='+OP_KEY;
-  console.log(apiAQUrl)
   fetch(apiAQUrl)
   .then(function (res) {
     if (res.ok) {
       res.json().then(function (data) {
         dataAQ=data
-        console.log(dataAQ)
-        //throwing air quality data into display function
       displayAirQuality(dataAQ)
     });
   }
 })
-//removed lon lat saving because doesn't work using locBtn
 };
 
 function displayAirQuality(dataAQ) {
